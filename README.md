ab1fd14be4ee3fa847b6d5e0fe25ff085f1f7347
Vincent Bruijn <vincent-bruijn@g-star.com>
2018-01-26 15:55:53 +0100

    adds more programs for propert testing, refactoring of project layout, additional sources added to readme

--- README.md
+++ README.md
@@ -21,13 +21,25 @@ Run the js-chef parser:
 $ node index.js
 ```
 
+## To do
+
+A lot of stuff needs to be done. Basically only processing integers to their ascii characters works. The rest is planned for the future. It will be ready when it's done.
 
 ## Resources
 
 * http://www.dangermouse.net/esoteric/chef.html
 * https://github.com/antlr/antlr4/blob/master/doc/javascript-target.md
 * https://github.com/maxbelanger/ratatouille/tree/master/grammar
+* http://p-helpers.appspot.com/chef/chef.html
 
+## Program resources
+
+* chef/hello-world.chef is taken from http://www.dangermouse.net/esoteric/chef.html
+* chef/fibonacci.chef is taken from http://www.dangermouse.net/esoteric/chef.html
+* chef/hello-world-cake.chef is taken from https://esolangs.org/wiki/Chef
+* chef/hello-world-2.chef is taken from http://progopedia.com/language/chef/
+* chef/factorial.chef is taken from http://progopedia.com/language/chef/
+* chef/fibonacci-2.chef is taken from http://progopedia.com/language/chef/
 
 ## License
 
--- index.js
+++ index.js
@@ -3,67 +3,37 @@
  * @license wtfpl v2
  * copyleft 2017 - no wrongs reversed
  */
-const antlr4 = require('antlr4');
 const fs = require('fs');
-const ChefLexer = require('./ChefLexer');
-const ChefParser = require('./ChefParser');
-const ChefListener = require('./ChefListener').ChefListener;
+const chefRunner = require('./lib/ChefRunner');
 
-var chefProgram = fs.readFileSync('./hello-world.chef').toString();
+const sourceDir = './chef';
+const chefRegExp = /^.*\.chef$/;
 
-var chars = new antlr4.InputStream(chefProgram);
-var lexer = new ChefLexer.ChefLexer(chars);
-var tokens  = new antlr4.CommonTokenStream(lexer);
-var parser = new ChefParser.ChefParser(tokens);
-parser.buildParseTrees = true;
+// read file content and pass to chefRunner
+function readAndProcessFile(fileName) {
+  fs.readFile(fileName, 'utf8', (error, content) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
 
-var tree = parser.program();
-
-ChefListenerHandler = function() {
-  ChefListener.call(this);
-  return this;
+    chefRunner.run(content);
+  });
 }
 
-ChefListenerHandler.prototype = Object.create(ChefListener.prototype);
-ChefListenerHandler.prototype.constructor = ChefListenerHandler;
-
-var listernerHandler = new ChefListenerHandler();
-
-var result = [];
-var variables = {};
-
-// output something
-ChefListenerHandler.prototype.exitServes = function(ctx) {
-  console.log(result.reverse().join(''));
-}
-
-ChefListenerHandler.prototype.enterIngredient = function(ctx) {
-  var ingredientLabel = ctx.children[ctx.children.length - 2].WORD().map(function(word) {
-    return word.getText();
-  }).join('-');
-  // console.log(ingredientLabel);
-  // console.log(ctx.children[0].INT().__proto__);
-  // console.log(ctx.children[0].INT().getText());
-
-  variables[ingredientLabel] = ctx.children[0].INT().getText();
-
-  // process.exit();
-
-};
-
-ChefListenerHandler.prototype.enterLiquefy = function(ctx) {
-  for (let i = 0; i < result.length; i++) {
-    result[i] = String.fromCharCode(result[i]);
+// read ./chef dir and pass to readAndProcessFile
+fs.readdir(sourceDir, (error, files) => {
+  if (error) {
+    console.log(error);
+    return;
   }
-};
 
+  for(let i = 0; i < files.length; i++) {
+    let file = files[i];
 
-ChefListenerHandler.prototype.enterPut = function(ctx) {
-  var ingredientLabel = ctx.children[1].WORD().map(function(word) {
-    return word.getText();
-  }).join('-');
-  result.push(variables[ingredientLabel]);
-};
-
-antlr4.tree.ParseTreeWalker.DEFAULT.walk(listernerHandler, tree);
-
+    if (chefRegExp.test(file)) {
+      let filePath = `${sourceDir}/${file}`;
+      readAndProcessFile(filePath);
+    }
+  }
+});
